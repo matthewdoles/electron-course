f1e78e1f9dfb2a28b2245d5b49c950c961ef9a7d
Matthew Doles <matteddied@gmail.com>
2020-09-01 19:05:40 -0400

    2.2 - Loading a Window File & Adding an Icon

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,11 +1,16 @@
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
     height: 600,
+    icon: './assets/icons/Icon_256x256.png',
   });
+
+  mainWindow.loadFile('./app/index.html');
 }
 
 app.on('ready', createMainWindow);
