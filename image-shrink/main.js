91b8bc0f8d8e6d39d167c213c6de92f07e35d920
Matthew Doles <matteddied@gmail.com>
2020-09-01 19:14:26 -0400

    2.3 - Environment & Platform Check

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 
+process.env.NODE_ENV = 'development';
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isPC = process.platform !== 'win32' ? true : false;
+const isMac = process.platform !== 'darwin' ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
@@ -7,10 +12,24 @@ function createMainWindow() {
     title: 'ImageShrink',
     width: 500,
     height: 600,
-    icon: './assets/icons/Icon_256x256.png',
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
   });
 
   mainWindow.loadFile('./app/index.html');
 }
 
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
+
 app.on('ready', createMainWindow);
+app.allowRendererProcessReuse = true;
