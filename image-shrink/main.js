d204c688e35a671b580d005daa9bbda309bcf055
Matthew Doles <matteddied@gmail.com>
2020-09-03 11:40:09 -0400

    4.6 - Send Events to Renderer & Electron Logs

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -5,8 +5,9 @@ const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const slash = require('slash');
+const log = require('electron-log');
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isPC = process.platform !== 'win32' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
@@ -96,10 +97,11 @@ const shinkImage = async ({ imgPath, quality, dest }) => {
       ],
     });
 
-    console.log(files);
+    log.info(files);
     shell.openPath(dest);
+    mainWindow.webContents.send('image:done');
   } catch (err) {
-    console.log(err);
+    log.error(err);
   }
 };
 
