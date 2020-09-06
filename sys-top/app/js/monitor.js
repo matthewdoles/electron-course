c42f2db4f62eeb1c8e2caac3ac4fa3a57154a072
Matthew Doles <matteddied@gmail.com>
2020-09-06 16:45:58 -0400

    5.4 - Dynamic System Stats

--- sys-top/app/js/monitor.js
+++ sys-top/app/js/monitor.js
@@ -4,14 +4,44 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+// run every two seconds
+setInterval(() => {
+  // cpu usage
+  cpu.usage().then((info) => {
+    document.getElementById('cpu-usage').innerText = info + '%';
+  });
+
+  // cpu free
+  cpu.free().then((info) => {
+    document.getElementById('cpu-free').innerText = info + '%';
+  });
+
+  // uptime
+  document.getElementById('sys-uptime').innerText = secondsToDHMS(os.uptime());
+}, 2000);
+
 // cpu model
 document.getElementById('cpu-model').innerText = cpu.model();
 
-//computer name
+// computer name
 document.getElementById('comp-name').innerText = os.hostname();
 
 // os
 document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
 
 // total memory
-mem.info().then(info => document.getElementById('mem-total').innerText = info.totalMemMb);
+mem
+  .info()
+  .then(
+    (info) => (document.getElementById('mem-total').innerText = info.totalMemMb)
+  );
+
+// show days, hours, mins, sec
+const secondsToDHMS = (seconds) => {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+};
